b872bb846cfd551ca1b94a5f582ba801ea2b4946
Tafadzwa Mashanda <novamashanda@gmail.com>
2026-09-19 20:46:47 +0200

    Add Bid/Ask imbalance with EMA smoothing and Ny session boundary

--- src/trading_algorithm/main.py
+++ src/trading_algorithm/main.py
@@ -40,5 +40,33 @@ def Cumulative_Volume_Delta_Calculator(Current_price,Ask_price,Bid_price, prices
             Cumulative_Volume_Delta += Volume_Delta
         print(Cumulative_Volume_Delta)
         return Cumulative_Volume_Delta
+    
+Previous_EMA = 0
+def bid_ask_imbalance(bid_depth, ask_depth, alpha, timestamp):
 
-Cumulative_Volume_Delta_Calculator(Current_price=10, Ask_price=12, Bid_price=19, prices=[98, 99, 100, 101], volume=10, timestamp=1459)
+    global Previous_EMA
+
+    imbalance = (bid_depth - ask_depth)/(bid_depth + ask_depth)
+    if imbalance > 0.5:
+        label = "Bid_Heavy"
+    elif imbalance < -0.5:
+        label = "Ask_Heavey"
+    else:
+        label = "Balanced"
+
+    if timestamp >= 2300:
+        EMA = 0
+        Previous_EMA = 0
+    elif timestamp >= 1530:
+        EMA = (alpha * imbalance) + ((1 - alpha) * Previous_EMA) 
+        Previous_EMA = 0
+        print(f"imbalance is {imbalance} | label is {label} | EMA is {EMA}")
+        return imbalance, label, EMA
+    else:
+        print(f"imbalance is {imbalance} | label is {label} | EMA is {None}")
+        return imbalance, label, None
+
+
+          
+
+    
